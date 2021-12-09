9f5f4101f5e86f5748f8c7798d8e005d5b82539c
Terminator-J <terminator_j@hotbutteredmonkey.com>
2021-12-09 09:04:32 -0800

    enchilada: vendor: Switch to source-built vendor
    Change-Id: I6889e1a38e823606e7e916193923469eae0c4a59
    
    Co-authored by: SevralT <a.chycherin@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,5 +22,8 @@ DEVICE_PATH := device/oneplus/enchilada
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # inherit from the proprietary version
 -include vendor/oneplus/enchilada/BoardConfigVendor.mk
